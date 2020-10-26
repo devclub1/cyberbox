573c7c8a4bf13c055a48d9ef685081fe1c62b1a8
axbg <bisagalexstefan@gmail.com>
2020-10-26 19:58:08 +0200

    Migrated ormconfig.json into code

--- back/src/models/db.ts
+++ back/src/models/db.ts
@@ -1,9 +1,21 @@
 import Container from 'typedi';
-import { ConnectionOptions, getConnectionOptions, createConnection, useContainer } from 'typeorm';
+import { createConnection, useContainer } from 'typeorm';
+import config from '../properties';
 
 export default async function establishDbConnection() {
     useContainer(Container);
 
-    const connectionOptions: ConnectionOptions = await getConnectionOptions();
-    return await createConnection(connectionOptions);
+    return await createConnection({
+        type: 'mysql',
+        port: config.DB_PORT,
+        host: config.DB_HOST,
+        database: config.DB_NAME,
+        username: config.DB_USER,
+        password: config.DB_PASSWORD,
+        synchronize: true,
+        logging: true,
+        entities: [
+            __dirname + "/*.js"
+        ]
+    });
 }
--- back/src/properties.ts
+++ back/src/properties.ts
@@ -1,14 +1,28 @@
 const dev = () => {
-    return {
-      DEV: true,
-      PORT: 8080,
-      COOKIE_SECRET: "SOME_SECRET_HERE",
-      AUTHENTICATION_ROUTE: '/api/authentication',
-      GOOGLE_CLIENT_ID: 'GOOGLE_CLIENT_ID',
-      GOOGLE_SECRET: 'GOOGLE_CLIENT_SECRET',
-      GITHUB_CLIENT_ID: 'GITHUB_CLIENT_ID',
-      GITHUB_CLIENT_SECRET: 'GITHUB_CLIENT_SECRET',
-    };
+  return {
+    DEV: true,
+    PORT: 8080,
+
+    DB_HOST: 'DB_HOST',
+    DB_PORT: 3306,
+    DB_NAME: 'DB_NAME',
+    DB_USER: 'DB_USER',
+    DB_PASSWORD: 'DB_PASSWORD',
+
+    COOKIE_SECRET: 'SOME_SECRET_HERE',
+    COOKIE_DURATION: 24 * 60 * 60 * 1000,
+    COOKIE_ACTIVE_DURATION: 1000 * 60 * 5,
+    COOKIE_SECURE_SETTING: false,
+
+    AUTHENTICATION_ROUTE: '/api/authentication',
+    GOOGLE_CALLBACK_URL: '/google/callback',
+    GITHUB_CALLBACK_URL: '/github/callback',
+
+    GOOGLE_CLIENT_ID: 'GOOGLE_CLIENT_ID',
+    GOOGLE_SECRET: 'GOOGLE_SECRET',
+    GITHUB_CLIENT_ID: 'GITHUB_CLIENT_ID',
+    GITHUB_CLIENT_SECRET: 'GITHUB_CLIENT_SECRET'
   };
+};
 
 export default dev();
