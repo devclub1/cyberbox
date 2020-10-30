c94302a77051eb6960d8582472a9e49805ed40af
axbg <bisagalexstefan@gmail.com>
2020-10-30 19:47:31 +0200

    Added lcov2badge into README.md

--- back/src/services/MockService.ts
+++ back/src/services/MockService.ts
@@ -1,9 +1,6 @@
 export default class MockService {
     public add(a: number, b: number) {
-        return a + b;
-    }
-
-    public divide(a: number, b: number) {
-        return b !== 0 ? a / b : 0;
+        const x = a + b;
+        return x;
     }
 }
