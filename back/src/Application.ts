adce42d1d9ef0d02c7a05763f7479535eafe07cb
axbg <bisagalexstefan@gmail.com>
2020-11-14 00:17:36 +0200

    Fixed SwaggerUI rendering

--- back/src/Application.ts
+++ back/src/Application.ts
@@ -6,6 +6,8 @@ import morgan from 'morgan';
 import Logger from './configurations/Logger';
 import { Action, useContainer, useExpressServer } from 'routing-controllers';
 import Passport from './configurations/Passport';
+import swaggerUI from 'swagger-ui-express';
+import yamljs from 'yamljs';
 import sessions from 'client-sessions';
 import AuthService from './services/AuthService';
 import { ErrorHandlerMiddleware } from './middlewares/ErrorHandlerMiddleware';
@@ -48,6 +50,8 @@ export class Application {
 
         this.instance.use(morgan('combined', this.logger.getMorganOptions()));
 
+        this.instance.use('/docs', swaggerUI.serve, swaggerUI.setup(yamljs.load('swagger.yaml')));
+
         useExpressServer(this.instance, {
             routePrefix: '/api',
             controllers: [
@@ -62,7 +66,6 @@ export class Application {
                 return await this.authService.getUserById(action.request.session.user.id);
             }
         });
-
     }
 
     public async start() {
