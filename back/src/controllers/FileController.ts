955a30e2b256d5c77e928bef799b7d7ff1b4fd5e
deodre <45459027+deodre@users.noreply.github.com>
2021-01-10 15:01:43 +0200

    Started working on file download controller, needs revision

--- back/src/controllers/FileController.ts
+++ back/src/controllers/FileController.ts
@@ -1,6 +1,8 @@
 import { Response } from 'express';
+import { join } from 'path';
 import { JsonController, Get, Param, Res } from 'routing-controllers';
 import { Inject } from 'typedi';
+import { promisify } from 'util';
 import FileService from '../services/FileService';
 
 @JsonController('/file')
@@ -14,6 +16,17 @@ export class FileController {
         const result = await this.fileService.getFileById(uuid);
         if (result !== undefined) {
             return res.status(200).send(result);
+        } else {
+            return res.status(404).json({message: 'File not found'});
+        }
+    }
+
+    @Get('/:uuid/download')
+    async downloadFile(@Param('uuid') uuid: string, @Res() res: Response) {
+        const result = await this.fileService.getFileById(uuid);
+        if (result !== undefined) {
+            const file = join(result.path, result.name + result.extension);
+            return await promisify<string, void>(res.download.bind(res))(file);
         } else {
             return res.status(404).json({message: 'File not found'});
         }
--- back/src/services/FileService.ts
+++ back/src/services/FileService.ts
@@ -1,12 +1,13 @@
 import { Service } from 'typedi';
 import { InjectRepository } from 'typeorm-typedi-extensions';
+import { File } from '../models/File';
 import FileRepository from '../repositories/FileRepository';
 
 @Service()
 export default class FileService {
     constructor(@InjectRepository() private fileRepository: FileRepository) { }
 
-    public async getFileById(uuid: string) {
+    public async getFileById(uuid: string): Promise<File> {
         return this.fileRepository.findOne(uuid);
     }
 }
